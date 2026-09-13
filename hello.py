614dcb2ef32e28654c3f53406be616d1d914bef5
AdityaThete57 <125346833@umail.ucc.ie>
2026-09-13 21:04:27 +0000

    added few bits to check ipad sync

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print('Hello to GIT World.')
 print('I am using the ipad!')
 print('Can you see me?')
+print('lets see how many times i need to commit, btw commit is basically writing a review of what you just wrote in the file.')
